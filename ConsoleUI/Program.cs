591fa5fe11f3eb7969c2b1769f143daf7636987d
BIKTOP <biktop512@gmail.com>
2023-06-19 13:37:44 +0300

    add project

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using TaskForCopyFiles;
+
+Console.WriteLine("Hello, World!");
+//var t = new TaskForCopy();
+//t.Execute();
---
+++ TaskForCopyFiles/TaskForCopy.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace TaskForCopyFiles
+{
+    public class TaskForCopy : Task
+    {
+        private string sourceDir;// = "C:\\From";
+        private string targetDir;// = "C:\\To";
+
+        //[Required]
+        public string SourceDir
+        {
+            get
+            {
+                return sourceDir;
+            }
+            set
+            {
+                sourceDir = value;
+            }
+        }
+
+        //[Required]
+        public string TargetDir
+        {
+            get
+            {
+                return targetDir;
+            }
+            set
+            {
+                targetDir = value;
+            }
+        }
+        public override bool Execute()
+        {
+            
+            Copy(sourceDir, targetDir);
+            return true;
+        }
+
+        void Copy(string source, string target)
+        {
+            Directory.CreateDirectory(target);
+
+            foreach (var file in Directory.GetFiles(source))
+                File.Copy(file, Path.Combine(target, Path.GetFileName(file)));
+
+            foreach (var directory in Directory.GetDirectories(source))
+                Copy(directory, Path.Combine(target, Path.GetFileName(directory)));
+        }
+    }
+}
